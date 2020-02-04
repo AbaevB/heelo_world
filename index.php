0b951f1210ab9980b1449e831e60b742963c17c1
markevillive <mark_evillive@hotmail.com>
2020-02-04 20:31:43 +0700

    Add CSS

--- index.php
+++ index.php
@@ -4,22 +4,25 @@
     <title>Hello, World</title>
     <meta charset="utf-8">
     <meta name="description" content="My first experience with GIT">
+    <link rel="stylesheet" href="css/style.css">
     </head>
   <body>
     <header>
       <h1>Hello, World!</h1>
     </header>
-    <div>
-    <h2>PHP example</h2>
-    <?php
-    $hello = "Hello, World!";
-    echo $hello;
-    ?>
-    </div>
+        <div>
+            <p>Вот пример работы php.</p>
+            <h2>PHP example</h2>
+            <?php
+            $hello = "Hello, World!";
+            echo $hello;
+            ?>
+            
+        </div>
     <footer>
-     <h5>
-     &copy;Андрей Баев 2020
-     </h5>
+         <h5>
+         &copy;Андрей Баев 2020
+         </h5>
     </footer>
   </body>
   
