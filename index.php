ff9359e59a2b599f402b759ee5a683f3acf12681
AbaevB <60654928+AbaevB@users.noreply.github.com>
2020-02-04 20:18:18 +0700

    Create index.php
    Создан файл index.php с примером вываода переменной.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello, World</title>
+    <meta charset="utf-8">
+    <meta name="description" content="My first experience with GIT">
+    </head>
+  <body>
+    <header>
+      <h1>Hello, World!</h1>
+    </header>
+    <div>
+    <h2>PHP example</h2>
+    <?php
+    $hello = "Hello, World!";
+    echo $hello;
+    ?>
+    </div>
+    <footer>
+     <h5>
+     &copy;Андрей Баев 2020
+     </h5>
+    </footer>
+  </body>
+  
+</html>
